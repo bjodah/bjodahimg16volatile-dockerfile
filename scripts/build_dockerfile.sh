ed9bb84569fe108c45ea50b59f11ea7bc3e7c31d
Björn Dahlgren <bjodah@gmail.com>
2018-02-14 15:12:15 +0100

    Latest cmake

--- scripts/build_dockerfile.sh
+++ scripts/build_dockerfile.sh
@@ -2,4 +2,3 @@ IMAGE=${1:-bjodahimg16volatile}
 TAG=${2:-latest}
 ABS_REPO_PATH=$(unset CDPATH && cd "$(dirname "$0")/.." && echo $PWD)
 docker build -t $IMAGE:$TAG ${ABS_REPO_PATH}/environment/
-
--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -5,8 +5,8 @@ ABS_REPO_PATH=$(unset CDPATH && cd "$(dirname "$0")/.." && echo $PWD)
 mkdir -p ${ABS_REPO_PATH}/match3-build
 docker run --rm --name bjodahimg16volatile-tests -e TERM -v ${ABS_REPO_PATH}/match3-build:/match3-build -it $IMAGE:$TAG \
        bash -c "\
-cd /opt/emsdk_portable && ./emsdk activate latest && . emsdk_env.sh && \
-cd /match3-build && which em++ && git clone --recursive git://github.com/modern-cpp-examples/match3.git && \
+cd /opt/emsdk-portable && ./emsdk activate latest && source ./emsdk_env.sh && \
+cd /match3-build && which em++ && touch foo && rm -r * && git clone --recursive git://github.com/bjodah/match3.git && \
 cmake -DCMAKE_CXX_COMPILER=em++ match3 && make web"
 
 if [ $? -ne 0 ]; then
